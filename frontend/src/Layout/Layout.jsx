fe084c31ea112340e92f5052cb1897f43280d162
Tonmoy <tonmoysutradhar2002@gmail.com>
2025-09-03 02:20:39 +0600

    Navbar design and navigate the link done.

--- frontend/src/Layout/Layout.jsx
+++ frontend/src/Layout/Layout.jsx
@@ -1,16 +1,17 @@
-import React from "react";
 import Header from "../components/Header/Header";
-import Routes from "../routes/Routes";
 import Footer from "../components/Footer/Footer";
+import Router from "../router/Router";
 
 function Layout() {
   return (
     <>
-      <Header></Header>
-      <main>
-        <Routes></Routes>
-      </main>
-      <Footer></Footer>
+      <div className=" px-32">
+        <Header></Header>
+        <main className="max-w-max h-[670px]">
+          <Router></Router>
+        </main>
+        <Footer></Footer>
+      </div>
     </>
   );
 }
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,9 +1,54 @@
-import React from "react";
+import logo from "../../assets/logo.png";
+import { NavLink, Link } from "react-router-dom";
 
 function Header() {
+  const navLinks = [
+    {
+      path: "/home",
+      display: "Home",
+    },
+    {
+      path: "/doctors",
+      display: "Find a Doctor",
+    },
+    {
+      path: "/services",
+      display: "Services",
+    },
+    {
+      path: "/contact",
+      display: "Contact",
+    },
+  ];
+
   return (
-    <div>
-      <h1>header</h1>
+    <div className="w-full max-h-max ">
+      <div className="flex items-center justify-between">
+        {/* logo */}
+        <div className=" ">
+          <img src={logo} alt="" />
+        </div>
+
+        {/* Menu */}
+        <div>
+          <ul className="menu flex items-center gap-3">
+            {navLinks.map((link, index) => (
+              <li key={index}>
+                <NavLink
+                  className={(navClass) =>
+                    navClass.isActive
+                      ? "text-blue-500 text-[16px] leading-7 font-bold"
+                      : "text-gray-500 text-[16px] leading-7 "
+                  }
+                  to={link.path}
+                >
+                  {link.display}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
